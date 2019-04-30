858a97da6cc7c87f44a53b0d3ee3d85aaae7b848
Aziraphaleo <45585678+Aziraphaleo@users.noreply.github.com>
2019-04-30 14:57:38 +0800

    Create chat.h

---
+++ chat.h
@@ -0,0 +1,15 @@
+//void calcu(char inputCh[255]);
+double Add(double a, double b);
+double Sub(double a, double b);
+double Mul(double a, double b);
+double Div(double a, double b);
+void calc(char ch[100]);
+double con(char a);
+void split( char **arr, char *str, const char *del);
+void ran(char **arr);
+void gettime(void);
+void getday(char **arr);
+int name(void);
+void smtalk(void);
+char cases(char *str);
+
