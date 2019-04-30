79f9e7fe1adbc10ca01e8a4d01916c18ba085180
Aziraphaleo <45585678+Aziraphaleo@users.noreply.github.com>
2019-04-30 14:59:20 +0800

    Create file.c

---
+++ file.c
@@ -0,0 +1,15 @@
+    char buf[MAX_LINE];  //缓冲区
+    FILE *fp;            //文件指针
+    int len;             //行字符个数
+    if((fp = fopen("test.txt","r")) == NULL)
+    {
+        perror("fail to read");
+        exit (1) ;
+    }
+    while(fgets(buf,MAX_LINE,fp) != NULL)
+    {
+        len = strlen(buf);
+        buf[len-1] = '\0';  //去掉换行符
+        printf("%s %d \n",buf,len - 1);
+    }
+    return NULL;
